e6c18b8596e1d9a12d4c340794167d7d391085a6
Kimberley Osborn <kimberleyfaith@Kimberleys-MBP.modem>
2021-06-08 06:51:11 +1000

    Update to home page v4

--- binnorie/config/application.rb
+++ binnorie/config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Binnorie
   class Application < Rails::Application
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
@@ -15,5 +16,6 @@ module Binnorie
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
   end
 end
